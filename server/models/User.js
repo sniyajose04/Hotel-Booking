390815ec5effdffaa02968f35f9e9d2c863fedcb
Sniya Jose <sniyajose18@Gmail.com>
2025-08-15 19:07:23 +0530

    mistake correction

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    _id: String, // allow Clerk's string ID
-    username: String,
-    email: String,
-    image: String,
+    _id: {type:String,required:true}, // allow Clerk's string ID
+    username: {type:String,required:true},
+    email: {type:String,required:true},
+    image: {type:String,required:true},
     role: { type: String, default: "user" },
     recentSearchedCities: [String],
     createdAt: { type: Date, default: Date.now },
